57170d58396e0d51b32a7efeaa111cda13ad8cc5
Abej Rijwi <43278687+Abejrijwi@users.noreply.github.com>
2024-12-11 09:16:55 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // Treat all JS code as newer version
+
+// alert (3 + 3) // We are using nodejs, Not Browser
+
+console.log(3 
+    + 
+    3); // Code redable should be high
+
+console.log("Abej Rijwi")
+
+let name = "Abej Rijwi"
+let age = 25
+let isLoggedIn = true
+let state;
+
+// number => 2 to power 53
+// biginit
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => undefined
+// symbol => unique
+// object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
+
