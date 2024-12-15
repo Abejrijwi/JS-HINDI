75b00ff47407aa7a43112dbee3e5199b52858d16
Abej Rijwi <43278687+Abejrijwi@users.noreply.github.com>
2024-12-15 14:07:30 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,28 @@
+const name = "Hitesh"
+const repoCount = 50
+// console.log(name + repoCount + " Value") // do not use this type of syntex
+
+console.log(`my name is ${name} and my repocount is ${repoCount}`)
+
+const gameName = new String ("hitesh-hc")
+// console.log(gameName[0])
+// console.log(gameName.__proto__)
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.toLowerCase())
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('i'))
+// const newString = gameName.substring(0, 5)
+// console.log(newString)
+// const anotherString = gameName.slice(-8, 9)
+// console.log(anotherString)
+
+// const newStringOne = "     Hitesh      "
+// console.log(newStringOne)
+// console.log(newStringOne.trim())
+
+const url = "http://hitesh.com/hitesh%20choudary"
+console.log(url.replace("%20", "-"))
+console.log(gameName.split("-"))
+
+
