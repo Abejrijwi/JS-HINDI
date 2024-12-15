25fcd8946bb06ed1a6fc73f6df2aaa6f32160e76
Abej Rijwi <43278687+Abejrijwi@users.noreply.github.com>
2024-12-15 05:25:46 +0000

    datatypes-summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -2,15 +2,15 @@
 
 // 7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInit
 
-const score = 100
-const scoreValue = 102.3
-const isLoggedIn = false
-const outsideTem = null
+const score = 100;
+const scoreValue = 102.3;
+const isLoggedIn = false;
+const outsideTem = null;
 let userEmail;
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
-const bigNumber = 57438658465n
+const id = Symbol("123");
+const anotherId = Symbol("123");
+const bigNumber = 57438658465n;
 
 // console.log(typeof bigNumber)
 
@@ -19,10 +19,32 @@ const bigNumber = 57438658465n
 
 const heros = ["Shaktiman", "Nagar", "Doga"];
 let myObj = {
-    name: "Abej Rijwi",
-    age: 22,
+  name: "Abej Rijwi",
+  age: 22,
 };
-const myFunction = function(){
-    console.log("Hello World")
+const myFunction = function () {
+  console.log("Hello World");
 };
-console.log(typeof myFunction)
+// console.log(typeof myFunction)
+
+// **************************************Memory************************************
+
+// Stack Memory (Primitive)
+
+let myYoutubename = "rijwi@google.com";
+let anotherName = myYoutubename;
+anotherName = "raju@gmail.com";
+console.log(myYoutubename)
+console.log(anotherName)
+
+// Heap Memory (Non-Primitive)
+
+let userOne = {
+    email: "love@gmail.com",
+    upi : "love@ypl.com"
+};
+let userTwo = userOne
+userTwo.email = "rakesh@google.com"
+console.log(userOne.email);
+console.log(userTwo.email);
+
