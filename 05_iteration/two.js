11d7a6856c8312c66fc8f9f574fb160c6cc06405
Abej Rijwi <43278687+Abejrijwi@users.noreply.github.com>
2024-12-28 09:49:58 +0000

    Iteration and While Loop

---
+++ 05_iteration/two.js
@@ -0,0 +1,36 @@
+//***************************************Iteration and While Loop********************************/
+
+let index = 0;
+
+while (index <= 10) {
+  //   console.log(`Value of Index ${index}`);
+  index = index + 1;
+}
+
+//***********************************************************************************************/
+
+let myArray = ["batman", "superman", "spiderman"];
+let arr = 0;
+
+while (arr < myArray.length) {
+  //   console.log(`Value is ${myArray[arr]}`);
+  arr = arr + 1;
+}
+
+//***************************************Iteration and do While Loop********************************/
+
+let score = 1;
+
+do {
+  //   console.log(`Score is ${score}`);
+  score++;
+} while (score <= 10);
+
+//*************************************************************************************************/
+
+let number = 8;
+
+do {
+  console.log(`Number is ${number}`);
+  number++;
+} while (number <= 5);
